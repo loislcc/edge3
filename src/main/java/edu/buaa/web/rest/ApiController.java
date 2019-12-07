0679154439853324c7e21a8bc7308499a3361847
lois <loisliuchang@163.com>
2019-12-07 18:34:46 +0800

    回传本机ip

--- src/main/java/edu/buaa/web/rest/ApiController.java
+++ src/main/java/edu/buaa/web/rest/ApiController.java
@@ -14,7 +14,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 import javax.servlet.http.HttpServletResponse;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -47,19 +50,70 @@ public class ApiController {
 
     @GetMapping("/detectTarget")
     public void detectTarget() {
-        InetAddress ia=null;
         TargetNotification targetNotification = new TargetNotification();
         SimpleDateFormat sdf = new SimpleDateFormat();
-        String localip=ia.getHostAddress();
+        String localip = getLocalIpAddr();
         targetNotification.setIp(localip);
         targetNotification.setCurrentTime(sdf.format(new Date()));
         targetNotification.setCategory("cat");
-        targetNotification.setLongitude(116.434925);
-        targetNotification.setLatitude(39.915672);
-        targetNotification.setSelfLongitude(116.35754);
-        targetNotification.setSelfLatitude(39.987037);
+        targetNotification.setLongitude(116.423745);
+        targetNotification.setLatitude(39.911124);
+//        targetNotification.setSelfLongitude(116.434924);
+//        targetNotification.setSelfLatitude(39.915671);116.433547
+        targetNotification.setSelfLongitude(116.433547);
+        targetNotification.setSelfLatitude(39.909462);
         updateTargetNotificationProducer.sendMsgToGateway(targetNotification);
     }
+
+    // 通用获取本机ip
+    public String getLocalIpAddr() {
+
+        String clientIP = null;
+        Enumeration<NetworkInterface> networks = null;
+        try {
+            //获取所有网卡设备
+            networks = NetworkInterface.getNetworkInterfaces();
+            if (networks == null) {
+                //没有网卡设备 打印日志  返回null结束
+//                log.debug("networks  is null");
+                return null;
+            }
+        } catch (SocketException e) {
+            System.out.println(e.getMessage());
+        }
+        InetAddress ip;
+        Enumeration<InetAddress> addrs;
+        // 遍历网卡设备
+        while (networks.hasMoreElements()) {
+            NetworkInterface ni = networks.nextElement();
+            try {
+                //过滤掉 loopback设备、虚拟网卡
+                if (!ni.isUp() || ni.isLoopback() || ni.isVirtual()) {
+                    continue;
+                }
+            } catch (SocketException e) {
+//                logger.info(e.getMessage());
+            }
+            addrs = ni.getInetAddresses();
+            if (addrs == null) {
+//                logger.info("InetAddress is null");
+                continue;
+            }
+            // 遍历InetAddress信息
+            while (addrs.hasMoreElements()) {
+                ip = addrs.nextElement();
+                if (!ip.isLoopbackAddress() && ip.isSiteLocalAddress() && ip.getHostAddress().indexOf(":") == -1) {
+                    try {
+                        clientIP = ip.toString().split("/")[1];
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        clientIP = null;
+                    }
+                }
+            }
+        }
+        return clientIP;
+    }
+
 }
 
 
