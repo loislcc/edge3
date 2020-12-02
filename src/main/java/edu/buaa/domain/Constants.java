11ed1bd5274eec350aa69581a4cc04a141205c28
lois <loisliuchang@163.com>
2020-12-02 21:29:09 +0800

    path

--- src/main/java/edu/buaa/domain/Constants.java
+++ src/main/java/edu/buaa/domain/Constants.java
@@ -2,9 +2,9 @@ package edu.buaa.domain;
 
 public class Constants {
 
-    public final static String filepathtosave = "/home/nano/project/ErasureCode/recive"; // 正在执行
+    public final static String filepathtosave = "/home/g410/project/ErasureCode/recive"; // 正在执行
 
-    public final static String filepathtosaveimg = "/home/nano/project/IMG/recive"; // 正在执行
+    public final static String filepathtosaveimg = "/home/g410/project/IMG/recive"; // 正在执行
 
     public final static String Edgename = "edge3";
 
